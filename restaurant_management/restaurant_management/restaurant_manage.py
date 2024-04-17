cc25516157a9ec8ded92eaf2eb42fdf0a503c308
Quantum Bit Core <qubitcore.io@gmail.com>
2024-04-17 18:00:38 -0400

    Update to version 1.8.1

--- restaurant_management/restaurant_management/restaurant_manage.py
+++ restaurant_management/restaurant_management/restaurant_manage.py
@@ -16,7 +16,7 @@ def check_exceptions(model, error_message):
             exceptions = frappe.get_single("Restaurant Settings")
             profile = frappe.db.get_value("User", frappe.session.user, "role_profile_name")
 
-            permissions = frappe.db.get_all("Restaurant Exceptions", fields=(
+            permissions = frappe.db.get_all("Restaurant Exceptions", parent_doctype="Restaurant Settings", fields=(
                 "order_write", "order_delete", "order_manage"
             ), filters={
                 "role_profile": profile
